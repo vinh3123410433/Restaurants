bebcc4bd5bd669a92c8b8d0c99f2158357f9cf7b
DESKTOP-I1MMTTE\KaBail <phong1112003@gmail.com>
2025-04-09 03:59:49 +0700

    lan 11

--- includes/header.php
+++ includes/header.php
@@ -53,7 +53,7 @@
         <!-- Navbar & Hero Start -->
         <div class="container-fluid position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
-                <a href="" class="navbar-brand p-0">
+                <a href="<?php echo APPURL;?>/index.php" class="navbar-brand p-0">
                     <h1 class="text-primary m-0"><i class="fa fa-utensils me-3"></i>Mộc Mộc Quán</h1>
                     <!-- <img src="img/logo.png" alt="Logo"> -->
                 </a>
--- menu.php
+++ menu.php
@@ -25,12 +25,12 @@
 
             <div class="container-fluid py-5 bg-dark hero-header mb-5">
                 <div class="container text-center my-5 pt-5 pb-4">
-                    <h1 class="display-3 text-white mb-3 animated slideInDown">Food Menu</h1>
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">Menu</h1>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb justify-content-center text-uppercase">
                             <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
                             <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                            <li class="breadcrumb-item text-white active" aria-current="page">Trang chủ</li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">Menu</li>
                         </ol>
                     </nav>
                 </div>
